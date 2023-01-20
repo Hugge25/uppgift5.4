04b0287333c86cf1b1ad73728760e7f5532a8220
Meysam03 <simon.munter@icloud.com>
2023-01-20 12:16:37 +0100

    Add project files.

---
+++ prg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[] {"Jesper", "Gabriel", "Fowzi", "Edvin", "Anton", "Hugo"};
+            Array.Sort(namn);
+
+            Console.WriteLine(namn[0]);
+
+        }
+    }
+}
